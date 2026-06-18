ebf1eb8a8e189fee72800f4f26c9fe7115f447d8
weidWilson22 <marckwilson27@gmail.com>
2026-06-18 03:42:16 -0400

    subida de archivos individuales

---
+++ EJ_4_ReconocimientoZonas/EJ_4_ReconocimientoZonas/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace EJ_4_ReconocimientoZonas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtNombreZona = new System.Windows.Forms.TextBox();
+            this.btnCargarMapa = new System.Windows.Forms.Button();
+            this.btnReconocer = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.rbGuardar = new System.Windows.Forms.RadioButton();
+            this.rbReconocer = new System.Windows.Forms.RadioButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(753, 488);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // txtNombreZona
+            // 
+            this.txtNombreZona.Location = new System.Drawing.Point(839, 165);
+            this.txtNombreZona.Name = "txtNombreZona";
+            this.txtNombreZona.Size = new System.Drawing.Size(167, 20);
+            this.txtNombreZona.TabIndex = 1;
+            // 
+            // btnCargarMapa
+            // 
+            this.btnCargarMapa.Location = new System.Drawing.Point(839, 46);
+            this.btnCargarMapa.Name = "btnCargarMapa";
+            this.btnCargarMapa.Size = new System.Drawing.Size(133, 23);
+            this.btnCargarMapa.TabIndex = 2;
+            this.btnCargarMapa.Text = "Cargar Mapa";
+            this.btnCargarMapa.UseVisualStyleBackColor = true;
+            this.btnCargarMapa.Click += new System.EventHandler(this.btnCargarMapa_Click);
+            // 
+            // btnReconocer
+            // 
+            this.btnReconocer.Location = new System.Drawing.Point(859, 227);
+            this.btnReconocer.Name = "btnReconocer";
+            this.btnReconocer.Size = new System.Drawing.Size(160, 23);
+            this.btnReconocer.TabIndex = 3;
+            this.btnReconocer.Text = "Reconocer Zona";
+            this.btnReconocer.UseVisualStyleBackColor = true;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(856, 292);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(83, 13);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "Zona detectada";
+            // 
+            // rbGuardar
+            // 
+            this.rbGuardar.AutoSize = true;
+            this.rbGuardar.Location = new System.Drawing.Point(829, 322);
+            this.rbGuardar.Name = "rbGuardar";
+            this.rbGuardar.Size = new System.Drawing.Size(63, 17);
+            this.rbGuardar.TabIndex = 5;
+            this.rbGuardar.TabStop = true;
+            this.rbGuardar.Text = "Guardar";
+            this.rbGuardar.UseVisualStyleBackColor = true;
+            // 
+            // rbReconocer
+            // 
+            this.rbReconocer.AutoSize = true;
+            this.rbReconocer.Location = new System.Drawing.Point(971, 322);
+            this.rbReconocer.Name = "rbReconocer";
+            this.rbReconocer.Size = new System.Drawing.Size(78, 17);
+            this.rbReconocer.TabIndex = 6;
+            this.rbReconocer.TabStop = true;
+            this.rbReconocer.Text = "Reconocer";
+            this.rbReconocer.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(756, 505);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 496);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.rbReconocer);
+            this.Controls.Add(this.rbGuardar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnReconocer);
+            this.Controls.Add(this.btnCargarMapa);
+            this.Controls.Add(this.txtNombreZona);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtNombreZona;
+        private System.Windows.Forms.Button btnCargarMapa;
+        private System.Windows.Forms.Button btnReconocer;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.RadioButton rbGuardar;
+        private System.Windows.Forms.RadioButton rbReconocer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ EJ_4_ReconocimientoZonas/EJ_4_ReconocimientoZonas/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EJ_4_ReconocimientoZonas
+{
+    public partial class Form1 : Form
+    {
+        string cadenaConexion = @"Server=DESKTOP-A8IF6A9\MSSQLSERVER01;Database=MapaZonasDB;Integrated Security=True;TrustServerCertificate=True;";
+        Bitmap mapaBitmap;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnCargarMapa_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Image img = Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = img;
+                mapaBitmap = new Bitmap(img);
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (mapaBitmap == null)
+            {
+                MessageBox.Show("carga la imagen.");
+                return;
+            }
+            Color colorPixel = mapaBitmap.GetPixel(e.X, e.Y);
+            if (rbGuardar.Checked)
+            {
+                string nombreZona = txtNombreZona.Text.Trim();
+                if (string.IsNullOrEmpty(nombreZona))
+                {
+                    MessageBox.Show("Escribe un nombre para la zona antes de hacer clic.");
+                    return;
+                }
+
+                GuardarMuestraEnBD(nombreZona, colorPixel);
+            }
+            else if (rbReconocer.Checked)
+            {
+                ReconocerZona(colorPixel);
+            }
+        }
+        private void GuardarMuestraEnBD(string nombreZona, Color color)
+        {
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                string query = "INSERT INTO MuestrasZonas (NombreZona, ValorR, ValorG, ValorB) VALUES (@Nombre, @R, @G, @B)";
+                SqlCommand comando = new SqlCommand(query, conexion);
+                
+                comando.Parameters.AddWithValue("@Nombre", nombreZona);
+                comando.Parameters.AddWithValue("@R", color.R);
+                comando.Parameters.AddWithValue("@G", color.G);
+                comando.Parameters.AddWithValue("@B", color.B);
+
+                try
+                {
+                    conexion.Open();
+                    comando.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al guardar en BD: " + ex.Message);
+                }
+            }
+        }
+        private void ReconocerZona(Color colorPixelClic)
+        {
+            Dictionary<string, int> conteoCoincidencias = new Dictionary<string, int>();
+            double umbralDistancia = 60.0; 
+
+            using (SqlConnection conexion = new SqlConnection(cadenaConexion))
+            {
+                string query = "SELECT NombreZona, ValorR, ValorG, ValorB FROM MuestrasZonas";
+                SqlCommand comando = new SqlCommand(query, conexion);
+
+                try
+                {
+                    conexion.Open();
+                    SqlDataReader reader = comando.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string nombreZonaBD = reader["NombreZona"].ToString();
+                        int rBD = Convert.ToInt32(reader["ValorR"]);
+                        int gBD = Convert.ToInt32(reader["ValorG"]);
+                        int bBD = Convert.ToInt32(reader["ValorB"]);
+
+                        double distancia = Math.Sqrt(Math.Pow(colorPixelClic.R - rBD, 2) +
+                                                     Math.Pow(colorPixelClic.G - gBD, 2) +
+                                                     Math.Pow(colorPixelClic.B - bBD, 2));
+
+                        if (distancia <= umbralDistancia)
+                        {
+                            if (conteoCoincidencias.ContainsKey(nombreZonaBD))
+                            {
+                                conteoCoincidencias[nombreZonaBD]++;
+                            } else
+                            {
+                                conteoCoincidencias.Add(nombreZonaBD, 1);
+                            }
+                        }
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+            if (conteoCoincidencias.Count > 0)
+            {
+                string zonaGanadora = "";
+                int maxCoincidencias = 0;
+                StringBuilder mensaje = new StringBuilder();
+                mensaje.AppendLine("Conteo de coincidencias por categoría:");
+
+                foreach (var item in conteoCoincidencias)
+                {
+                    mensaje.AppendLine(string.Format("-{0}: {1} coincidencias",item.Key,item.Value));
+
+                    if (item.Value > maxCoincidencias)
+                    {
+                        maxCoincidencias = item.Value;
+                        zonaGanadora = item.Key;
+                    }
+                }
+                lblResultado.Text = "Zona detectada: " + zonaGanadora;
+                MessageBox.Show(mensaje.ToString(), "Resultado del Reconocimiento", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                lblResultado.Text = "Zona detectada: Desconocida";
+                MessageBox.Show( "Sin resultados");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
